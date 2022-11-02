7f45cc55404fbe510b288b360a58343a81fe017d
Ramiro M Hermes <rmhermes94@gmail.com>
2022-11-02 09:09:54 -0300

    react

---
+++ clase22/src/App.js
@@ -0,0 +1,20 @@
+/* opcional si no arranca ---  
+import React from 'react'; */
+
+
+import Header from './componentes/layout/Header'
+
+
+function App() {
+  return (
+    <div className ="App">
+      <Header>
+        
+      </Header>
+      {/* opcion simple */}<Header/>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ clase22/src/componentes/layout/Header.js
@@ -0,0 +1,7 @@
+const Header = (props) =>{
+    return(
+        <p> Hola soy el header </p>
+    )
+    }
+
+    export default Header;
