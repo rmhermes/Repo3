c5576f3daa281913cb0ad1a53376bdb4295c6807
Ramiro M Hermes <rmhermes94@gmail.com>
2022-11-09 10:06:48 -0300

    fin clase 24

--- clase22/src/pages/Ejemplo4.js
+++ clase22/src/pages/Ejemplo4.js
@@ -1,7 +1,19 @@
+import alumnos from '../data/alumnos.json';
 
-const Ejemplo4 =props =>{
-    return(
-        <h1>HOLA SOY PAG 4</h1>
+const Ejemplo4 = props => {
+    return (
+        <>
+            <h1>HOLA SOY PAG 4</h1>
+            <ul>
+                {alumnos.map(alumno => (
+                    <li>
+                        {alumno.nombre} {alumno.apellido} - ({alumno.edad})
+                    </li>
+                )
+                )}
+            </ul>
+
+        </>
     )
 }
 export default Ejemplo4;
